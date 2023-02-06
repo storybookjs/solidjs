6c19372afc776fce8b1000693484891ae144fc31
Mauricio Rivera <mauriver21@gmail.com>
2023-02-06 16:04:09 -0500

    Added js cli template

--- packages/renderers/solid/template/cli/js/Button.stories.jsx
+++ packages/renderers/solid/template/cli/js/Button.stories.jsx
@@ -1,13 +1,41 @@
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/html/writing-stories/introduction
-export default {
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/solid/writing-stories/introduction
+const meta = {
   title: 'Example/Button',
+  component: Button,
   tags: ['autodocs'],
-  render: (args) => <Button {...args}/>,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/html/writing-stories/args
+export default meta;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/solid/writing-stories/args
 export const Primary = {
-  args: {},
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
 };
