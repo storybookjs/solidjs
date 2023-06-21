9b2be81cabdba431b8d8cba3bd823def86ff62a2
Sinyoung "Divinespear" Kang <divinespear@gmail.com>
2023-06-21 20:02:22 +0000

    encapsule stores per context

--- packages/renderers/solid/src/render.tsx
+++ packages/renderers/solid/src/render.tsx
@@ -1,6 +1,6 @@
 import type { Component } from 'solid-js';
 import { ErrorBoundary, onMount } from 'solid-js';
-import { SetStoreFunction, createStore, reconcile } from 'solid-js/store';
+import { createStore, reconcile } from 'solid-js/store';
 import { render as solidRender } from 'solid-js/web';
 import type {
   RenderContext,
@@ -8,63 +8,124 @@ import type {
   Args,
   StoryFn,
 } from '@storybook/types';
-import type { ComponentsData, SolidRenderer, StoryContext } from './types';
+import type { ContextStore, ComponentsData, SolidRenderer, StoryContext } from './types';
 
 /**
- * SolidJS store for handling fine grained updates
- * of the components data as f.e. story args.
+ * encapsule store per context
  */
-const componentStores: {
-  [id: string]: [get: ComponentsData, set: SetStoreFunction<ComponentsData>];
+const CONTEXT_STORES: {
+  [id: string]: ContextStore;
 } = {};
-// const [store, setStore] = createStore({} as ComponentsData);
-let store: ComponentsData;
-let setStore: SetStoreFunction<ComponentsData>;
 
-const getStore = (componentId: string): typeof componentStores[string] => 
-  componentStores[componentId] ||= createStore({} as ComponentsData);
+function createContextStore(context: StoryContext<SolidRenderer, Args>): ContextStore {
+  /**
+   * SolidJS store for handling fine grained updates
+   * of the components data as f.e. story args.
+   */
+  const [store, setStore] = createStore({} as ComponentsData);
+
+  let { globals, componentId, viewMode } = context;
+
+  /**
+   * Checks when the story requires to be remounted.
+   * Elements outside the story requires a whole re-render.
+   * e.g. dark theme, show grid, etc...
+   */
+  const remount: ContextStore['remount'] = (force, context) => {
+    let flag = false;
+  
+    // Story view mode has changed
+    if (viewMode !== context.viewMode) flag = true;
+  
+    // Force flag is set to true.
+    if (force) flag = true;
+  
+    // Globals refers to storybook visualization options.
+    if (!Object.is(globals, context.globals)) flag = true;
+  
+    // Story main url id has changed
+    if (componentId !== context.componentId) {
+      flag = true;
+      unmountAll();
+    }
+  
+    // Global values are updated when remount is true
+    if (flag === true) {
+      viewMode = context.viewMode;
+      globals = context.globals;
+      componentId = context.componentId;
+    }
+  
+    return flag;
+  };
+
+  /**
+   * All stories are disposed.
+   */
+  const disposeAllStories = () => {
+    Object.keys(store).forEach((storyId) => {
+      store[storyId]?.disposeFn?.();
+    });
+  };
+
+  /**
+   * Resets reactive store
+   */
+  const cleanStore = () => {
+    setStore(reconcile({}));
+  };
+
+  /**
+   * Unmounts all the store and rendered solid apps
+   */
+  const unmountAll = () => {
+    disposeAllStories();
+    cleanStore();
+  };
+
+  /**
+   * Resets an specific story store.
+   */
+  const cleanStoryStore = (storeId: string) => {
+    setStore({ [storeId]: { args: {}, rendered: false, disposeFn: () => {} } });
+  };
+
+  /**
+   * Disposes an specific story.
+   */
+  const disposeStory = (storeId: string) => {
+    store[storeId]?.disposeFn?.();
+  };
+
+  /**
+   * This function resets the canvas and reactive store for an specific story.
+   */
+  const remountStory = (storyId: string) => {
+    disposeStory(storyId);
+    cleanStoryStore(storyId);
+  };
+
+  /**
+   * Checks if the story store exists
+   */
+  const storyIsRendered = (storyId: string) => Boolean(store[storyId]?.rendered);
+
+  return {
+    store,
+    setStore,
+    remount,
+    remountStory,
+    storyIsRendered,
+  };
+}
+
+function contextStore(context: StoryContext<SolidRenderer, Args>): ContextStore {
+  return (CONTEXT_STORES[context.componentId] ||= createContextStore(context));
+}
 
 // Delay util fn
 const delay = async (ms: number = 20) => {
   await new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-// Global variables
-let globals: StoryContext<SolidRenderer>['globals']; //Storybook view configurations.
-let componentId: string; //Unique component story id.
-let viewMode: string; //It can be story or docs.
-
-/**
- * Checks when the story requires to be remounted.
- * Elements outside the story requires a whole re-render.
- * e.g. dark theme, show grid, etc...
- */
-const remount = (force: boolean, context: StoryContext<SolidRenderer>) => {
-  let flag = false;
-
-  // Story view mode has changed
-  if (viewMode !== context.viewMode) flag = true;
-
-  // Force flag is set to true.
-  if (force) flag = true;
-
-  // Globals refers to storybook visualization options.
-  if (!Object.is(globals, context.globals)) flag = true;
-
-  // Story main url id has changed
-  if (componentId !== context.componentId) {
-    flag = true;
-    unmountAll();
-  }
-
-  // Global values are updated when remount is true
-  if (flag === true) {
-    viewMode = context.viewMode;
-    globals = context.globals;
-    componentId = context.componentId;
-  }
-
-  return flag;
 };
 
 /**
@@ -75,7 +136,7 @@ export const solidReactivityDecorator = (
   storyFn: StoryFn<SolidRenderer, Args>,
   context: StoryContext<SolidRenderer>
 ) => {
-  const [store] = getStore(context.componentId);
+  const { store } = contextStore(context);
   let storyId = context.canvasElement.id;
   context.args = store[storyId].args;
   return storyFn(context.args as Args & StoryContext<SolidRenderer>, context);
@@ -107,57 +168,6 @@ export const render: ArgsStoryFn<SolidRenderer> = (_, context) => {
 };
 
 /**
- * All stories are disposed.
- */
-let disposeAllStories = () => {
-  Object.keys(store).forEach((storyId) => {
-    store[storyId]?.disposeFn?.();
-  });
-};
-
-/**
- * Resets reactive store
- */
-const cleanStore = () => {
-  setStore(reconcile({}));
-};
-
-/**
- * Unmounts all the store and rendered solid apps
- */
-const unmountAll = () => {
-  disposeAllStories();
-  cleanStore();
-};
-
-/**
- * Resets an specific story store.
- */
-const cleanStoryStore = (storeId: string) => {
-  setStore({ [storeId]: { args: {}, rendered: false, disposeFn: () => {} } });
-};
-
-/**
- * Disposes an specific story.
- */
-const disposeStory = (storeId: string) => {
-  store[storeId]?.disposeFn?.();
-};
-
-/**
- * This function resets the canvas and reactive store for an specific story.
- */
-const remountStory = (storyId: string) => {
-  disposeStory(storyId);
-  cleanStoryStore(storyId);
-};
-
-/**
- * Checks if the story store exists
- */
-const storyIsRendered = (storyId: string) => Boolean(store[storyId]?.rendered);
-
-/**
  * Checks if the story is in docs mode.
  */
 const isDocsMode = (context: StoryContext<SolidRenderer, Args>) =>
@@ -169,11 +179,11 @@ const isDocsMode = (context: StoryContext<SolidRenderer, Args>) =>
 const renderSolidApp = (
   storyId: string,
   renderContext: RenderContext<SolidRenderer>,
-  canvasElement: SolidRenderer['canvasElement']
+  canvasElement: SolidRenderer['canvasElement'],
+  setStore: ContextStore['setStore'],
 ) => {
   const { storyContext, unboundStoryFn, showMain, showException } =
     renderContext;
-  const [_, setStore] = getStore(storyContext.componentId);
 
   setStore(storyId, 'rendered', true);
 
@@ -211,13 +221,8 @@ export async function renderToCanvas(
   let forceRemount = renderContext.forceRemount;
   let storyId = storyContext.canvasElement.id;
 
-  // Initializes global default values for checking remounting.
-  viewMode = storyContext.viewMode;
-  globals = storyContext.globals;
-  componentId = storyContext.componentId;
-
-  // init store/setStore
-  [store, setStore] = getStore(componentId);
+  // Initializes
+  const { setStore, remount, remountStory, storyIsRendered } = contextStore(storyContext);
 
   // Story is remounted given the conditions.
   if (remount(forceRemount, storyContext)) {
@@ -233,7 +238,7 @@ export async function renderToCanvas(
     // for rendering all the stories in docs mode when global changes.
     if (isDocsMode(storyContext)) await delay();
 
-    const disposeFn = renderSolidApp(storyId, renderContext, canvasElement);
+    const disposeFn = renderSolidApp(storyId, renderContext, canvasElement, setStore);
     setStore(storyId, (prev) => ({ ...prev, disposeFn }));
   }
 }
