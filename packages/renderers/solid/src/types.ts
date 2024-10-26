5b5498958a4b7f92bad1082bba9e984e5ab3b8ea
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2024-10-26 13:14:15 -0400

    Update types.ts

--- packages/renderers/solid/src/types.ts
+++ packages/renderers/solid/src/types.ts
@@ -5,6 +5,7 @@ export type { RenderContext } from '@storybook/types';
 export type { StoryContext } from '@storybook/types';
 
 export interface SolidRenderer extends WebRenderer {
+  // @ts-expect-error Component expects a record, but TArgs are unknown.
   component: Component<this['T']>;
   storyResult: StoryFnSolidReturnType;
 }
