b5346198cbfe6589afa72724c7cc8ae979172e1d
Tegan Churchill <tegan@slime.camp>
2025-04-04 05:39:13 -0700

    Fix preset.js 'module not defined' (#20)
    * Treat as commonjs
    
    * Bumped versions
    
    Modified versions of packages and dependencies in order to make sure they works across different versions of peer dependencies and was using the latest.
    
    ---------
    
    Co-authored-by: Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,16 @@ import tseslint from 'typescript-eslint';
 import * as depend from 'eslint-plugin-depend';
 
 export default [
+  {
+    rules: {
+      'depend/ban-dependencies': [
+        'error',
+        {
+          allowed: ['fs-extra'],
+        },
+      ],
+    },
+  },
   { ignores: ['**/dist/'] },
   { files: ['**/*.{ts,tsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
